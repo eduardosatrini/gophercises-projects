af040e1e441fe93de39ca8f9252a5f417da69b3f
Eduardo Henrique <eduardosatrini@gmail.com>
2021-05-11 19:07:44 -0300

    refatoring

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -7,6 +7,19 @@ import (
 	"os"
 )
 
+func checkErr(err error, msg string) {
+
+	if err != nil {
+		exitMsg(msg)
+	}
+}
+
+func exitMsg(m string) {
+
+	fmt.Println(m)
+	os.Exit(1)
+}
+
 func ConfigFlag() (CSVFileName *string) {
 
 	CSVFileName = flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
@@ -15,24 +28,14 @@ func ConfigFlag() (CSVFileName *string) {
 	return
 }
 
-func ExitMsg(m string) {
-
-	fmt.Println(m)
-	os.Exit(1)
-}
-
 func OpenCSVFile(fileName string) [][]string {
 
 	file, err := os.Open(fileName)
-	if err != nil {
-		ExitMsg("Failed to open the CSV file.")
-	}
+	checkErr(err, "Failed to open the CSV file.")
 
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
-	if err != nil {
-		ExitMsg("Failed to parse the provided CSV file.")
-	}
+	checkErr(err, "Failed to parse the provided CSV file.")
 
 	return lines
 }
